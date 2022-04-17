0cee24587f3e653efe1975eb9804f71e470f4359
karim092 <71761680+karim092@users.noreply.github.com>
2022-04-17 20:03:00 +0300

    lesson_4

---
+++ Lesson_4/Lesson_4/Lesson_4.cpp
@@ -0,0 +1,59 @@
+﻿#include <vector>
+#include <list>
+#include <math.h>
+#include <algorithm>
+
+#include "templates.hpp"
+using namespace std;
+
+void task_1()
+{
+    const unsigned int size = 10;
+    int newIntValue = -3;
+    double newDoubleValue = 2.2;
+
+    vector<int> v(size);
+    generator(v, -10, 10);
+    sort(v.begin(), v.end());
+    print(v);                             // -9 -9 -4 3 3 4 6 8 9 10
+    insert_sorted(v, newIntValue);
+    print(v);                             // -9 -9 -4 -3 3 3 4 6 8 9 10
+    cout << endl;
+
+    list<double> l(size);
+    generator(l, -10.0, 10.0);
+    l.sort();
+    print(l);                             // -9.31992 -6.76971 -5.98545 -2.1297 -1.9722 -0.292701 0.715274 3.43089 4.35249 9.45377
+    insert_sorted(l, newDoubleValue);
+    print(l);                             // -9.31992 -6.76971 -5.98545 -2.1297 -1.9722 -0.292701 0.715274 2.2 3.43089 4.35249 9.45377
+    cout << endl;
+}
+
+void task_2()
+{
+    vector<double> v(100);
+
+    double error = 0;
+    generator(v, -1000.0, 1000.0);
+
+    print(v);                                                          // 1-ый вектор - печать исходного вектора
+    copy(v.begin(), v.end(), ostream_iterator<int> { cout, " " });     // 2-ой вектор - печать целочисленного вектора
+    cout << endl;
+
+    for_each(v.begin(), v.end(), [&](const double& i)
+        {
+            error += pow(i - static_cast<int>(i), 2);
+        });
+
+    cout << endl << "Ошибка между цифровым и аналоговым сигналом равна " << error << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    task_1();
+    task_2();
+
+    return 0;
+}
---
+++ Lesson_4/Lesson_4/templates.hpp
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <iostream>
+#include <random>
+#include <iterator>
+template<template<typename, typename> class TContainer, typename TItem, typename TIterator, typename T>
+void generator(TContainer<TItem, TIterator>& c, const T& rangeStart, const T& rangeEnd)
+{
+    std::random_device rd;
+    std::mt19937 mersenne(rd());
+    std::uniform_real_distribution<double> urd(rangeStart, rangeEnd);
+    generate(c.begin(), c.end(), [&]()
+        {
+            return urd(mersenne);
+        });
+}
+
+template<template<typename, typename> class TContainer, typename TItem, typename TIterator>
+void print(const TContainer<TItem, TIterator>& c)
+{
+    copy(c.begin(), c.end(), std::ostream_iterator<TItem> { std::cout, " " });
+    std::cout << std::endl;
+}
+
+template<template<typename, typename> class TContainer, typename TItem, typename TIterator>
+void insert_sorted(TContainer<TItem, TIterator>& c, const TItem& i)
+{
+    typename TContainer<TItem, TIterator>::iterator it = find_if(c.begin(), c.end(), [&](const TItem& current)
+        {
+            return current >= i;
+        });
+    c.insert(it, i);
+}
